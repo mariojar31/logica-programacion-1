89d125da2ba1d479a221e525c762fbea580102f4
Mario Acendra <mariojoacre31@gmail.com>
2024-09-05 14:17:10 -0500

    Primer commit

---
+++ practica.js
@@ -0,0 +1,14 @@
+let num1=parseInt(prompt("Ingrese el número 1: "))
+let num2=parseInt(prompt("Ingrese el número 2: "))
+let num3=parseInt(prompt("Ingrese el número 3: "))
+
+menor=Math.min(num1,num2,num3)
+mayor=Math.max(num1,num2,num3)
+medio=[num1,num2,num3].sort()[1]
+
+// console.log(`${menor},${medio},${mayor}`)
+// console.log(`${mayor},${medio},${menor}`)
+
+alert(`${menor},${medio},${mayor}\n${mayor},${medio},${menor}`)
+
+
---
+++ practicaOpc2.js
@@ -0,0 +1,9 @@
+let numbers=[]
+numbers[0]=parseInt(prompt("Ingrese numero 1: "))
+numbers[1]=parseInt(prompt("Ingrese numero 2: "))
+numbers[2]=parseInt(prompt("Ingrese numero 3: "))
+
+// console.log(numbers.sort())
+// console.log(numbers.reverse())
+
+alert(`${numbers.sort()}\n${numbers.reverse()}`)
