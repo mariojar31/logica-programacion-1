90717d320be8b3ee56c51044d631b346614be15f
Mario Acendra <mariojoacre31@gmail.com>
2024-09-06 09:46:57 -0500

    .

--- practica.js
+++ practica.js
@@ -6,8 +6,8 @@ menor=Math.min(num1,num2,num3)
 mayor=Math.max(num1,num2,num3)
 medio=[num1,num2,num3].sort()[1]
 
-// console.log(`${menor},${medio},${mayor}`)
-// console.log(`${mayor},${medio},${menor}`)
+console.log(`${menor},${medio},${mayor}`)
+console.log(`${mayor},${medio},${menor}`)
 
 alert(`${menor},${medio},${mayor}\n${mayor},${medio},${menor}`)
 
--- practicaOpc2.js
+++ practicaOpc2.js
@@ -3,7 +3,7 @@ numbers[0]=parseInt(prompt("Ingrese numero 1: "))
 numbers[1]=parseInt(prompt("Ingrese numero 2: "))
 numbers[2]=parseInt(prompt("Ingrese numero 3: "))
 
-// console.log(numbers.sort())
-// console.log(numbers.reverse())
+console.log(numbers.sort())
+console.log(numbers.reverse())
 
 alert(`${numbers.sort()}\n${numbers.reverse()}`)
